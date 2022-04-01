07a27062aab813ce07adcfa5c629f4040736f791
Larissa de Campos <97813649+larissadecampos@users.noreply.github.com>
2022-04-01 15:22:28 -0300

    Delete blogPessoal directory

--- blogPessoal/snippet/Snippet.java
+++
@@ -1,8 +0,0 @@
-package snippet;
-
-public class Snippet {
-	public static void main(String[] args) {
-		userLogin.get().setFoto(usuario.get().getFoto());
-	}
-}
-
--- blogPessoal/src/test/java/org/generation/blogPessoal/repository/package-info.java
+++
@@ -1,1 +0,0 @@
-package org.generation.blogPessoal.repository;
